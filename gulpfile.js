3a516c4c9bc59938be9709742f1dfae90a2eac87
Luc <luc@boissaye.fr>
2015-11-12 17:46:36 +0100

    Test sass.

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var paths = {
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+  gulp.src('./sass/ionic.app.sass')
     .pipe(sass())
     .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
